8a2a7f6e91f136e9c5a92b96ce6482d59a530c77
Elijah M <nicelife24@gmail.com>
2019-02-07 16:01:22 -0500

    First commit

---
+++ Assignment 1/js/main.js
@@ -0,0 +1,40 @@
+"use strict";
+
+window.onload = function() {
+    // You can copy-and-paste the code from any of the examples at http://examples.phaser.io here.
+    // You will need to change the fourth parameter to "new Phaser.Game()" from
+    // 'phaser-example' to 'game', which is the id of the HTML element where we
+    // want the game to go.
+    // The assets (and code) can be found at: https://github.com/photonstorm/phaser/tree/master/examples/assets
+    // You will need to change the paths you pass to "game.load.image()" or any other
+    // loading functions to reflect where you are putting the assets.
+    // All loading functions will typically all be found inside "preload()".
+    
+    var game = new Phaser.Game( 800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update } );
+    var tileSet;
+	
+    function preload() {
+        // Load an image and call it 'logo'.
+        game.load.image( 'logo', 'assets/phaser.png' );
+		game.load.image('sky', 'assets/sky.png');
+    }
+    
+    //var bouncy;
+    
+    function create() {
+        
+		tileSet = game.add.tileSprite(0, 0, 800, 600, 'sky');
+		
+    }
+    
+    function update() {
+        // Accelerate the 'logo' sprite towards the cursor,
+        // accelerating at 500 pixels/second and moving no faster than 500 pixels/second
+        // in X or Y.
+        // This function returns the rotation angle that makes it visually match its
+        // new trajectory.
+        //bouncy.rotation = game.physics.arcade.accelerateToPointer( bouncy, game.input.activePointer, 500, 500, 500 );
+		
+		
+    }
+};
